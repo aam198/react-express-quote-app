7035772a932e49975e75c877c01a16f42605ff26
aam198 <aam198@psu.edu>
2022-11-26 12:00:46 -0500

    Component: Quotes build

---
+++ frontend/src/Quotes.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, {useState} from "react";
+
+function Quotes() {
+  const [ text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function getQuote() {
+    // make our HTTP request to our backend
+    axios.get("http://localhost:5000/", {crossdomain: true})
+    .then(response => {
+      // Get object from response.data
+      setText(response.data.text);
+      setAuthor(response.data.author);
+    });
+  }
+
+  return (
+    <div> 
+      {/* Button to request Quote from backend */}
+      <button onClick={getQuote}>
+        Show Me a Quote!
+      </button>
+      {/* Prop of text that will be filled with quote from backend */}
+      <h1>{text}</h1>
+      <h3>{"-" + author} </h3>
+    </div>
+  )
+}
+
+export default Quotes;
