6b4eb4b39d5150a855397bd74b1be344491decde
aam198 <aam198@psu.edu>
2022-11-26 12:12:14 -0500

    CORS policy add

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,14 @@ const Quote = require('inspirational-quotes');
 
 const app = express();
 
+
+// CORS Policy add
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get("/", function(req, res) {
   // To send when home route is accessed or requested from frontend
   res.send(Quote.getQuote());
